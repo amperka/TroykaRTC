01bff2d9657aedfdb0da5088ab5fc290ee00195f
acosinwork <acosinwork@yandex.ru>
2015-09-18 13:42:33 +0300

    create Library

---
+++ TroykaRTC.cpp
@@ -0,0 +1,313 @@
+/****************************************************************************/	
+//	Function: Cpp file for troyka-RTC
+//	Hardware: DS1307
+//	Arduino IDE: Arduino-1.6.5
+//	Author:	 Igor Dementiev	
+//	Date: 	 Sep 10,2015
+//	Version: v1.0
+//	by www.amperka.ru
+/****************************************************************************/
+
+#include <Wire.h>
+#include "TroykaRTC.h"
+#include <EEPROM.h>
+
+
+uint8_t RTC::decToBcd(uint8_t val)
+{
+	return ( (val/10*16) + (val%10) );
+}
+
+//Convert binary coded decimal to normal decimal numbers
+uint8_t RTC::bcdToDec(uint8_t val)
+{
+	return ( (val/16*10) + (val%16) );
+}
+
+void RTC::begin()
+{
+	Wire.begin();
+}
+/*Function: The clock timing will start */
+void RTC::start()        // set the ClockHalt bit low to start the rtc
+{
+  Wire.beginTransmission(DS1307_I2C_ADDRESS);
+  Wire.write((uint8_t)0x00);                      // Register 0x00 holds the oscillator start/stop bit
+  Wire.endTransmission();
+  Wire.requestFrom(DS1307_I2C_ADDRESS, 1);
+  _second = Wire.read() & 0x7f;       // save actual seconds and AND sec with bit 7 (sart/stop bit) = clock started
+  Wire.beginTransmission(DS1307_I2C_ADDRESS);
+  Wire.write((uint8_t)0x00);
+  Wire.write((uint8_t)_second);                    // write seconds back and start the clock
+  Wire.endTransmission();
+}
+/*Function: The clock timing will stop */
+void RTC::stop()         // set the ClockHalt bit high to stop the rtc
+{
+  Wire.beginTransmission(DS1307_I2C_ADDRESS);
+  Wire.write((uint8_t)0x00);                      // Register 0x00 holds the oscillator start/stop bit
+  Wire.endTransmission();
+  Wire.requestFrom(DS1307_I2C_ADDRESS, 1);
+  _second = Wire.read() | 0x80;       // save actual seconds and OR sec with bit 7 (sart/stop bit) = clock stopped
+  Wire.beginTransmission(DS1307_I2C_ADDRESS);
+  Wire.write((uint8_t)0x00);
+  Wire.write((uint8_t)_second);                    // write seconds back and stop the clock
+  Wire.endTransmission();
+}
+/****************************************************************/
+/*Function: Read time and date from RTC	*/
+void RTC::read()
+{
+    // Reset the register pointer
+	Wire.beginTransmission(DS1307_I2C_ADDRESS);
+	Wire.write((uint8_t)0x00);
+	Wire.endTransmission();  
+	Wire.requestFrom(DS1307_I2C_ADDRESS, 7);
+	// A few of these need masks because certain bits are control bits
+	_second	   = bcdToDec(Wire.read() & 0x7f);
+	_minute	   = bcdToDec(Wire.read());
+	_hour	   = bcdToDec(Wire.read() & 0x3f);// Need to change this if 12 hour am/pm
+	_dayOfWeek  = bcdToDec(Wire.read());
+	_day = bcdToDec(Wire.read());
+	_month      = bcdToDec(Wire.read());
+	_year	   = bcdToDec(Wire.read()) + 2000;
+}
+/*******************************************************************/
+/*Frunction: Write the time that includes the date to the RTC chip */
+void RTC::set(uint8_t hour, uint8_t minute, uint8_t second, uint16_t day, uint8_t month, uint8_t year, uint8_t dow)
+{
+	_hour = hour;
+	_minute = minute;
+	_second = second;
+	_day = day;
+	_month = month;
+	_year = year-2000;
+	_dayOfWeek = dow;
+
+	Wire.beginTransmission(DS1307_I2C_ADDRESS);
+	Wire.write((uint8_t)0x00);
+	Wire.write(decToBcd(_second));// 0 to bit 7 starts the clock
+	Wire.write(decToBcd(_minute));
+	Wire.write(decToBcd(_hour));  // If you want 12 hour am/pm you need to set bit 6 
+	Wire.write(decToBcd(_dayOfWeek));
+	Wire.write(decToBcd(_day));
+	Wire.write(decToBcd(_month));
+	Wire.write(decToBcd(_year));
+	Wire.endTransmission();
+}
+
+void RTC::set(const char* compileTimeStamp)
+{
+	int i = 0;
+//	Serial.println(compileTimeStamp);
+
+	/*--------------------DayOfWeek----------------------------*/
+	char dow[2];
+	for(i = 0; i < 3; i++)
+		dow[i] =  compileTimeStamp[i];
+	dow[i] = '\0';
+	
+	if (strcmp(dow, "Mon") == 0)
+		_dayOfWeek = 1;
+	else if (strcmp(dow, "Tue") == 0)
+		_dayOfWeek = 2;
+	else if (strcmp(dow, "Wed") == 0)
+		_dayOfWeek = 3;	
+	else if (strcmp(dow, "Thu") == 0)
+		_dayOfWeek = 4;
+	else if (strcmp(dow, "Fri") == 0)
+		_dayOfWeek = 5;
+	else if (strcmp(dow, "Sat") == 0)
+		_dayOfWeek = 6;
+	else if (strcmp(dow, "Sun") == 0)
+		_dayOfWeek = 7;
+
+	/*--------------------------Day----------------------------*/
+	_day = ((compileTimeStamp[8] - '0') * 10 + (compileTimeStamp[9] - '0'));
+
+	/*--------------------------Month---------------------------*/
+	char month[2];
+	for(i = 0; i < 3; i++)
+		month[i] =  compileTimeStamp[i+4];
+	month[i] = '\0';
+
+	if (strcmp(month, "Jan") == 0)
+		_month = 1;
+	else if (strcmp(month, "Feb") == 0)
+		_month = 2;
+	else if (strcmp(month, "Mar") == 0)
+		_month = 3;	
+	else if (strcmp(month, "Apr") == 0)
+		_month = 4;
+	else if (strcmp(month, "May") == 0)
+		_month = 5;
+	else if (strcmp(month, "Jun") == 0)
+		_month = 6;
+	else if (strcmp(month, "Jul") == 0)
+		_month = 7;
+	else if (strcmp(month, "Aug") == 0)
+		_month = 8;
+	else if (strcmp(month, "Sep") == 0)
+		_month = 9;
+	else if (strcmp(month, "Oct") == 0)
+		_month = 10;
+	else if (strcmp(month, "Nov") == 0)
+		_month = 11;
+	else if (strcmp(month, "Dec") == 0)
+		_month = 12;
+
+	/*--------------------------Year----------------------------*/
+	_year = ((compileTimeStamp[20] - '0') * 1000 + (compileTimeStamp[21] - '0') * 100 + (compileTimeStamp[22] - '0') * 10  + (compileTimeStamp[23] - '0')) - 2000;
+	/*--------------------------Time----------------------------*/
+	_hour = ((compileTimeStamp[11] - '0') * 10 + (compileTimeStamp[12] - '0'));
+	_minute = ((compileTimeStamp[14] - '0') * 10 + (compileTimeStamp[15] - '0'));
+	_second = ((compileTimeStamp[17] - '0') * 10 + (compileTimeStamp[18] - '0'));
+
+	unsigned int hash =  _hour * 60 * 60 + _minute  * 60 + _second;
+
+	if (EEPROMReadInt(0) != hash) {
+		EEPROMWriteInt(0, hash);	
+		Wire.beginTransmission(DS1307_I2C_ADDRESS);
+		Wire.write((uint8_t)0x00);
+		Wire.write(decToBcd(_second));// 0 to bit 7 starts the clock
+		Wire.write(decToBcd(_minute));
+		Wire.write(decToBcd(_hour));  // If you want 12 hour am/pm you need to set bit 6 
+		Wire.write(decToBcd(_dayOfWeek));
+		Wire.write(decToBcd(_day));
+		Wire.write(decToBcd(_month));
+		Wire.write(decToBcd(_year));
+		Wire.endTransmission();
+	}
+}
+
+void RTC::getTimeStr(char* output, uint8_t len) const
+{
+	char buff[8];
+	if (_hour < 10)
+		buff[0] = '0';
+	else
+		buff[0] = char((_hour / 10)+ 48);
+	buff[1] = char((_hour % 10) + 48);
+	buff[2] = ':';
+
+	if (_minute < 10)
+		buff[3] = '0';
+	else
+		buff[3] = char((_minute / 10) + 48);
+	buff[4] = char((_minute % 10) + 48);
+	buff[5] = ':';
+
+	if (_second < 10) 
+		buff[6] = '0';
+	else
+		buff[6] = char((_second / 10) + 48);
+	buff[7] = char((_second % 10) + 48);
+	buff[8] = '\0';
+
+	len--;
+	int i = 0;
+	while (i < 8 && len > 0) {
+		output[i] = buff[i];
+		i++;
+		len--;
+	}
+	output[i] = '\0';
+	
+
+}
+
+void RTC::getDateStr(char* output, uint8_t len) const
+{
+	char buff[10];
+	if (_day < 10)
+		buff[0] = '0';
+	else
+		buff[0] = char((_day / 10)+ 48);
+	buff[1] = char((_day % 10) + 48);
+	buff[2] = '.';
+
+	if (_month < 10)
+		buff[3] = '0';
+	else
+		buff[3] = char((_month / 10) + 48);
+	buff[4] = char((_month % 10) + 48);
+	buff[5] = '.';
+	buff[6]=char((_year / 1000)+48);
+	buff[7]=char(((_year % 1000) / 100)+48);
+	buff[8]=char(((_year % 100) / 10)+48);
+	buff[9]=char((_year % 10)+48);
+	buff[10]='\0';
+	
+	len--;
+	int i = 0;
+	while (i < 10 && len > 0) {
+		output[i] = buff[i];
+		i++;
+		len--;
+	}
+	output[i] = '\0';
+
+}
+
+void RTC::getDOWStr(char* output, uint8_t len) const
+{
+
+	char buff[10];
+	switch (_dayOfWeek)
+	{
+		case MONDAY: {
+			strcpy(buff, "Monday");
+			break;
+		}
+		case TUESDAY: {
+			strcpy(buff, "Tuesday");
+			break;
+		}
+		case WEDNESDAY: {
+			strcpy(buff, "Wednesday");
+			break;
+		}
+		case THURSDAY: {
+			strcpy(buff,"Thursday");
+			break;
+		}
+		case FRIDAY: {
+			strcpy(buff, "Friday");
+			break;
+		}
+		case SATURDAY: {
+			strcpy(buff, "Saturday");
+			break;
+		}
+		case SUNDAY: {
+			strcpy(buff,"Sunday");
+			break;
+		}
+	}
+
+	int i = 0;
+	len--;
+	int s = strlen(buff);
+	while (i < s && len > 0) {
+		output[i] = buff[i];
+		i++;
+		len--;
+	}
+	output[i] = '\0';
+}
+
+//Запись двухбайтового числа в память
+void EEPROMWriteInt(int address, int value)
+{
+  EEPROM.write(address, lowByte(value));
+  EEPROM.write(address + 1, highByte(value));
+}
+ 
+//Чтение числа из памяти
+unsigned int EEPROMReadInt(int address)
+{
+  byte lowByte = EEPROM.read(address);
+  byte highByte = EEPROM.read(address + 1);
+ 
+  return (highByte << 8) | lowByte;
+}
---
+++ TroykaRTC.h
@@ -0,0 +1,65 @@
+/****************************************************************************/	
+//	Function: Header file for troyka-RTC
+//	Hardware: DS1307
+//	Arduino IDE: Arduino-1.6.5
+//	Author:	 Igor Dementiev	
+//	Date: 	 Sep 10,2015
+//	Version: v1.0
+//	by www.amperka.ru
+/****************************************************************************/
+#ifndef __troyka_RTC_H__
+#define __troyka_RTC_H__
+
+#include <Arduino.h>
+#define DS1307_I2C_ADDRESS 0x68
+
+#define MONDAY 		1
+#define TUESDAY 	2
+#define WEDNESDAY 	3
+#define THURSDAY 	4
+#define FRIDAY 		5
+#define SATURDAY 	6
+#define SUNDAY 		7
+
+class RTC
+{
+public:
+	void begin();
+	void start();
+	void stop();
+	void set(uint8_t hour, uint8_t minute, uint8_t second, uint16_t day, uint8_t month, uint8_t year, uint8_t dow);
+	void set(const char *compileTimeStamp);
+	void read();
+	void getTimeStr(char* output, uint8_t len) const;
+	void getDateStr(char* output, uint8_t len) const;
+	void getDOWStr(char* output, uint8_t len) const;
+    
+    uint8_t getSecond() const { return _second; }
+    uint8_t getMinute() const { return _minute; }
+    uint8_t getHour() const { return _hour; }
+    uint8_t getDOW() const { return _dayOfWeek; }
+    uint8_t getDay() const { return _day; }
+    uint8_t getMonth() const { return _month; }
+    uint16_t getYear() const { return _year; }
+private:
+	uint8_t decToBcd(uint8_t val);
+	uint8_t bcdToDec(uint8_t val);
+
+	uint8_t _second;
+	uint8_t _minute;
+	uint8_t _hour; 
+	uint8_t _dayOfWeek;// day of week, 1 = Monday
+	uint8_t _day;
+	uint8_t _month;
+	uint16_t _year;
+};
+
+
+//Запись двухбайтового числа в память
+void EEPROMWriteInt(int address, int value);
+
+ 
+//Чтение числа из памяти
+unsigned int EEPROMReadInt(int address);
+
+#endif
